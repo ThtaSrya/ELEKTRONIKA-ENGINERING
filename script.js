d52cc123b2ebf70424bec23a777af41716b5cdfe
ThtaSrya <148206094+ThtaSrya@users.noreply.github.com>
2024-10-13 18:35:33 +0700

    Update script.js
    Fix incorrect script.js

--- script.js
+++ script.js
@@ -22,6 +22,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Menambahkan event listener untuk ikon lokasi
     locationLink.addEventListener('click', function(e) {
         e.preventDefault();
-        window.open('https://www.google.com/maps/place/SMK+Negeri+1+Glagah/@-8.3530041,114.3303703,17z/data=!3m1!4b1!4m6!3m5!1s0x2dd679b8657e36f:0xe898d18942dcdada!8m2!3d-8.3530041!4d114.332559!16s%2Fg%2F11d_4v9jx0', '_blank');
+        window.open('https://www.google.com/maps/place/SMK+Negeri+1+Glagah+Banyuwangi/@-8.2155967,114.3418447,17z/data=!3m1!4b1!4m6!3m5!1s0x2dd14551b9fce1c7:0x18e9b60ff5b2fafe!8m2!3d-8.2155967!4d114.3444196!16s%2Fg%2F11bwdx20cp?entry=ttu&g_ep=EgoyMDI0MTAwOS4wIKXMDSoASAFQAw%3D%3D', '_blank');
     });
 });
